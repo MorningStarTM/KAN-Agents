8e3ee1212364cd5a26ec7fa9148c64810f036304
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-05-31 11:05:13 +0530

    kan layer created

--- agents/kan.py
+++ agents/kan.py
@@ -184,3 +184,43 @@ class KANNeuron(nn.Module):
             regularize_activation * regularization_loss_activation
             + regularize_entropy * regularization_loss_entropy
         )
+    
+
+
+class KANLayer(nn.Module):
+    def __init__(self, layer_hidden, grid_size=5, spline_order=3, scale_noise=0.1,
+                 scale_base=1.0, scale_spline=1.0, base_activation=nn.SiLU, grid_eps=0.02, grid_range=[-1, 1]):
+        super(KANLayer, self).__init__()
+        self.grid_size = grid_size
+        self.spline_order = spline_order
+
+        self.layers = nn.ModuleList()
+        for in_features, out_features in zip(layer_hidden, layer_hidden[1:]):
+            self.layers.append(
+                KANLayer(
+                    in_features,
+                    out_features,
+                    grid_size=grid_size,
+                    spline_order=spline_order,
+                    scale_noise=scale_noise,
+                    scale_base=scale_base,
+                    scale_spline=scale_spline,
+                    base_activation=base_activation,
+                    grid_eps=grid_eps,
+                    grid_range=grid_range,
+                )
+            )
+
+    
+    def forward(self, x: torch.Tensor, update_grid=False):
+        for layer in self.layers:
+            if update_grid:
+                layer.update_grid(x)
+            x = layer(x)
+        return x
+
+    def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
+        return sum(
+            layer.regularization_loss(regularize_activation, regularize_entropy)
+            for layer in self.layers
+        )
