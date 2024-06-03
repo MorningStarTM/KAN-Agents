b719a307b0e4e10e4aa714dab1e4683d07e8b602
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-06-03 12:37:21 +0530

    actorcritic network created

--- agents/actor_critic.py
+++ agents/actor_critic.py
@@ -6,38 +6,19 @@ from .kan import KANLayer
 import os
 from utils import plotLearning
 
-class GenericNetwork(nn.Module):
-    def __init__(self, HP:dict):
-        """
-        Args:
-            HP: dict - (alpha, input_dim, fc1, fc2, n_action)
-        """
-        super(GenericNetwork, self).__init__()
-        self.input_dims = HP['input_dim']
-        self.fc1_dim = HP['fc1']
-        self.fc2_dim = HP['fc2']
-        self.n_actions = HP['n_action']
-        self.fc1 = nn.Linear(self.input_dims, self.fc1_dim)
-        self.fc2 = nn.Linear(self.fc1_dim, self.fc2_dim)
-        self.fc3 = nn.Linear(self.fc2_dim, self.n_actions)
-        self.optimizer = optim.Adam(self.parameters(), lr=HP['lr'])
-
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.to(self.device)
-
-    def forward(self, state):
-        state = torch.tensor(state).to(self.device)
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
 
 class KANActorCriticNetwork(nn.Module):
     def __init__(self, input_dims=8, fc1=16, fc2=32, n_actions=4, lr=0.0003):
         """
+        This class for build actor acritic network based on KAN layer
+
         Args:
             HP: dict - (alpha, input_dim, fc1, fc2, n_action)
+
+        Returns:
+            pi (tensor) - policy
+            v (tensor) - value
+
         """
         super(KANActorCriticNetwork, self).__init__()
         self.input_dims = input_dims
@@ -61,8 +42,45 @@ class KANActorCriticNetwork(nn.Module):
     
 
 
+class ActorCriticNetwork(nn.Module):
+    def __init__(self, input_dims=8, fc1=16, fc2=8, n_actions=4, lr=0.0003):
+        """
+        This class for build actor acritic network 
 
-class ACAgent(object):
+        Args:
+            HP: dict - (alpha, input_dim, fc1, fc2, n_action)
+
+        Returns:
+            pi (tensor) - policy
+            v (tensor) - value
+        """
+        super(ActorCriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dim = fc1
+        self.fc2_dim = fc2
+        self.n_actions = n_actions
+        self.fc1 = nn.Linear(input_dims, self.fc1_dim)
+        self.fc2 = nn.Linear(self.fc1_dim, self.fc2_dim)
+        self.pi = nn.Linear(self.fc2_dim, self.n_actions)
+        self.v = nn.Linear(self.fc2_dim, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr, weight_decay=1e-4)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+    def forward(self, observation):
+        state = torch.Tensor(observation).to(self.device)
+        x = self.fc1(state)
+        pi = self.pi(x)
+        v = self.v(x)
+        return (pi, v)
+    
+
+    
+
+
+
+class KANACAgent(object):
     """ Agent class for use with a single actor critic network that shares
         the lowest layers. For use with more complex environments such as
         the discrete lunar lander
@@ -110,40 +128,49 @@ class ACAgent(object):
 
 
 
+class ACAgent(object):
+    """ Agent class for use with a single actor critic network that shares
+        the lowest layers. For use with more complex environments such as
+        the discrete lunar lander
+    """
+    def __init__(self, alpha, input_dims, gamma=0.99,
+                 layer1_size=16, layer2_size=32, action_dim=2):
+        self.gamma = gamma
+        self.actor_critic = ActorCriticNetwork()
 
-class Trainer:
-    def __init__(self, agent, env, epochs):
-        """
-        This is class for training agent.
+        self.log_probs = None
 
-        Args:
-            agent 
-            env : currently supports gym env
-            epochs (int)
-        """
-        self.agent = agent
-        self.env = env
-        self.epochs = epochs
-        self.history = []
-        self.score = []
+    def choose_action(self, observation):
+        probabilities, _ = self.actor_critic.forward(observation)
+        probabilities = F.softmax(probabilities, dim=-1)
+        action_probs = torch.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        log_probs = action_probs.log_prob(action)
+        self.log_probs = log_probs
+
+        return action.item()
+
+    def learn(self, state, reward, new_state, done):
+        self.actor_critic.optimizer.zero_grad()
+
+        _, critic_value_ = self.actor_critic.forward(new_state)
+        _, critic_value = self.actor_critic.forward(state)
+        reward = torch.tensor(reward, dtype=torch.float).to(self.actor_critic.device)
+
+        delta = reward + self.gamma*critic_value_*(1-int(done)) - critic_value
+
+        actor_loss = -self.log_probs * delta
+        critic_loss = delta**2
+
+        (actor_loss + critic_loss).backward()
+
+        self.actor_critic.optimizer.step()
 
     
-    def train(self):
-        score = 0
-        for i in range(self.epochs):
-            done = False
-            score = 0
-            observation, _ = self.env.reset()
+    def save_model(self, path):
+        os.makedirs(path, exist_ok=True)
+        torch.save(self.actor_critic.state_dict(), os.path.join(path, "actor_critic.pth"))
 
-            while not done:
-                action = self.agent.choose_action(observation)
-                observation_, reward, done, _ = self.env.step(action)
-                self.agent.learn(observation, reward, observation_, done)
-                observation = observation
-                score += reward
+    def load_model(self, path):
+        self.actor_critic.load_state_dict(torch.load(path))
 
-            self.history.append(score)
-            print(f"Episode {i} Score {score}")
-        
-        filename = "result.png"
-        plotLearning(self.history, filename=filename, window=50)
