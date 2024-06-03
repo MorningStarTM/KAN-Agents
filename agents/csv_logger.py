6343971755216eb3d64fa568f534e7e46d64d405
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-06-03 14:41:57 +0530

    csv logger created

---
+++ agents/csv_logger.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+
+class CSVLogger:
+    def __init__(self, filename, agent, **kwargs):
+        self.filename = filename
+        self.agent = agent
+        self.epochs = kwargs.get('epochs', 0)
+        self.convergence_point = kwargs.get('c_point', 0)
+
+    def log(self):
+        data = pd.DataFrame({'f1':self.agent.fc1_dims,
+                             'f2':self.agent.fc2_dims,
+                             'alpha':self.agent.alpha,
+                             'epochs':self.epochs,
+                             'c_point':self.convergence_point}
+                             )
+        
+        data.to_csv("result.csv")
