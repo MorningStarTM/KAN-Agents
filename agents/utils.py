9cd77b0ae2aaf62fa5ab4cb3cb631afc78376d75
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-06-03 14:01:15 +0530

    error fixed

--- agents/utils.py
+++ agents/utils.py
@@ -2,15 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plotLearning(scores, filename, x=None, window=5):   
-    N = len(scores)
+def plot_learning(history, filename, window=50):
+    """
+    Plot the learning curve.
+
+    Args:
+        history: The list of scores.
+        filename: The name of the file to save the plot.
+        window: The window size for the moving average.
+    """
+    N = len(history)
+    if N == 0:
+        print("Empty history provided. No plot will be generated.")
+        return
+    
     running_avg = np.empty(N)
     for t in range(N):
-        running_avg[t] = np.mean(scores[max(0, t-window):(t+1)])
-            
-    if x is None:
-        x = [i for i in range(N)]
-    plt.ylabel('Score')       
-    plt.xlabel('Game')                     
-    plt.plot(x, running_avg)
+        running_avg[t] = np.mean(history[max(0, t - window):(t + 1)])
+    
+    plt.plot(running_avg)
+    plt.title('Running average of previous {} scores'.format(window))
+    plt.xlabel('Episode')
+    plt.ylabel('Score')
+    plt.grid(True)
+    
     plt.savefig(filename)
+    plt.show()
