f36ccbe6d64002fd91d93974352a2584082232fe
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-27 23:33:43 +0530

    sac file created and replaye buffer added

---
+++ agents/sac.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class ReplayBuffer():
+    def __init__(self, max_size, input_shape, n_actions):
+        self.mem_size = max_size
+        self.mem_cent = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
+        self.action_memory = np.zeros((self.mem_size, n_actions))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool_)
+
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cent % self.mem_size
+
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+        
+        self.mem_cent += 1
+
+    
