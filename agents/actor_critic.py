9d8279d2365cf2c130098e01c0d9002530f11fc6
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-01 18:34:08 +0530

    actor critic updated

--- agents/actor_critic.py
+++ agents/actor_critic.py
@@ -68,7 +68,29 @@ class ActorCriticNetwork(nn.Module):
         return (pi, v)
     
 
-    
+
+class GenericNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions):
+        super(GenericNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.fc1 = nn.Linear(self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, n_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cuda:1')
+        self.to(self.device)
+
+    def forward(self, observation):
+        state = torch.tensor(observation, dtype=torch.float).to(self.device)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+        
 
 
 
@@ -124,39 +146,34 @@ class KANACAgent(object):
 
 
 
-class ACAgent(object):
-    """ Agent class for use with a single actor critic network that shares
-        the lowest layers. For use with more complex environments such as
-        the discrete lunar lander
+class DiscreteActorCritic(object):
+    """ Agent class for use with separate actor and critic networks.
+        This is appropriate for very simple environments, such as the mountaincar
     """
-    def __init__(self, alpha=0.0003, input_dims=4, gamma=0.99,
-                 layer1_size=16, layer2_size=32, action_dim=2):
-        self.name = "Vanilla Actor-Critic Agent"
+    def __init__(self, alpha, beta, input_dims, gamma=0.99,
+                 layer1_size=64, layer2_size=64, n_actions=2):
         self.gamma = gamma
-        self.fc1_dims = layer1_size
-        self.fc2_dims = layer2_size
-        self.alpha = alpha
-        
-        self.actor_critic = ActorCriticNetwork(alpha=self.alpha, input_dims=input_dims, fc1_dims=layer1_size, fc2_dims=layer2_size, n_actions=action_dim)
-
+        self.actor = GenericNetwork(alpha, input_dims, layer1_size,
+                                    layer2_size, n_actions=n_actions)
+        self.critic = GenericNetwork(beta, input_dims, layer1_size,
+                                     layer2_size, n_actions=1)
         self.log_probs = None
 
     def choose_action(self, observation):
-        probabilities, _ = self.actor_critic.forward(observation)
-        probabilities = F.softmax(probabilities, dim=-1)
+        probabilities = F.softmax(self.actor.forward(observation.unsqueeze(0)))
         action_probs = torch.distributions.Categorical(probabilities)
         action = action_probs.sample()
-        log_probs = action_probs.log_prob(action)
-        self.log_probs = log_probs
+        self.log_probs = action_probs.log_prob(action)
 
         return action.item()
 
     def learn(self, state, reward, new_state, done):
-        self.actor_critic.optimizer.zero_grad()
+        self.actor.optimizer.zero_grad()
+        self.critic.optimizer.zero_grad()
 
-        _, critic_value_ = self.actor_critic.forward(new_state)
-        _, critic_value = self.actor_critic.forward(state)
-        reward = torch.tensor(reward, dtype=torch.float).to(self.actor_critic.device)
+        critic_value_ = self.critic.forward(new_state.unsqueeze(0))
+        critic_value = self.critic.forward(state.unsqueeze(0))
+        reward = torch.tensor(reward, dtype=torch.float).to(self.actor.device)
 
         delta = reward + self.gamma*critic_value_*(1-int(done)) - critic_value
 
@@ -165,40 +182,20 @@ class ACAgent(object):
 
         (actor_loss + critic_loss).backward()
 
-        self.actor_critic.optimizer.step()
+        self.actor.optimizer.step()
+        self.critic.optimizer.step()
 
-    
     def save_model(self, path):
         os.makedirs(path, exist_ok=True)
-        torch.save(self.actor_critic.state_dict(), os.path.join(path, "actor_critic.pth"))
+        torch.save(self.actor.state_dict(), os.path.join(path, "actor.pth"))
+        torch.save(self.critic.state_dict(), os.path.join(path, "critic.pth"))
 
     def load_model(self, path):
-        self.actor_critic.load_state_dict(torch.load(path))
+        self.actor.load_state_dict(torch.load(path))
+        self.critic.load_state_dict(torch.load(path))
+        print("model loaded")
 
 
-
-class GenericNetwork(nn.Module):
-    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims,
-                 n_actions):
-        super(GenericNetwork, self).__init__()
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cuda:1')
-        self.to(self.device)
-
-    def forward(self, observation):
-        state = torch.tensor(observation, dtype=torch.float).to(self.device)
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        return x
 
 
 class ContinousActorCritic(object):
@@ -223,4 +220,3 @@ class ContinousActorCritic(object):
         return action.item()
     
 
-    
