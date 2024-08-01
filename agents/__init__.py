97adc2966de6dba088fc2e5bae8e5efb6b2fdc3e
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-01 20:52:53 +0530

    discrete AC added

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,5 @@
-from .actor_critic import KANActorCriticNetwork, ActorCriticNetwork
-from .actor_critic import ACAgent, KANACAgent
+from .actor_critic import KANActorCriticNetwork, ActorCriticNetwork, DiscreteActorCritic
+from .actor_critic import KANACAgent
 from .trainer import Trainer, PPOTrainer, QTrainer
 from .kan import KANLayer
 from .csv_logger import CSVLogger
