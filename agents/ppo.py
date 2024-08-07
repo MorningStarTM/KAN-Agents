2d6f4af0943fc9dcca3fa95de9960fa02a34f996
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-07 08:07:01 +0530

    re-creating ppo - buffer created

--- agents/ppo.py
+++ agents/ppo.py
@@ -1,47 +1,32 @@
 import os
+import glob
+import time
+from datetime import datetime
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import numpy as np
-from torch.distributions.categorical import Categorical
+from torch.distributions import MultivariateNormal
+from torch.distributions import Categorical
+
 from .kan import KANLayer
 
-class PPOMemory:
-    def __init__(self, batch_size):
-        self.states = []
-        self.probs = []
-        self.vals = []
+class RolloutBuffer:
+    def __init__(self):
         self.actions = []
+        self.states = []
+        self.logprobs = []
         self.rewards = []
-        self.dones = []
-
-        self.batch_size = batch_size
-
-    def generate_batches(self):
-        n_states = len(self.states)
-        batch_start = np.arange(0, n_states, self.batch_size)
-        indices = np.arange(n_states, dtype=np.int64)
-        np.random.shuffle(indices)
-        batches = [indices[i:i+self.batch_size] for i in batch_start]
-
-        return np.array(self.states), np.array(self.actions), np.array(self.probs),np.array(self.vals),np.array(self.rewards), np.array(self.dones), batches
+        self.state_values = []
+        self.is_terminals = []
     
-    def store_memory(self, state, action, probs, vals, reward, done):
-        self.states.append(state)
-        self.actions.append(action)
-        self.probs.append(probs)
-        self.vals.append(vals)
-        self.rewards.append(reward)
-        self.dones.append(done)
 
-    def clear_memory(self):
-        self.states = []
-        self.probs = []
-        self.actions = []
-        self.rewards = []
-        self.dones = []
-        self.vals = []
+    def clear(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.state_values[:]
+        del self.is_terminals[:]
 
 
 
