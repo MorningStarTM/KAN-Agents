1e9398de31f9ae74a704297aed4327b0557bd839
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-02-16 23:13:59 +0530

    updated

--- agents/ppo.py
+++ agents/ppo.py
@@ -6,7 +6,10 @@ import torch.optim as optim
 import numpy as np
 from torch.distributions.categorical import Categorical
 from torch.distributions import Normal
+from utils.logger import CustomLogger
+from .kan import KANLayer
 
+logger = CustomLogger(log_dir="logs", log_file_prefix="experiment")
 
 class PPOMemory:
     def __init__(self, batch_size):
@@ -50,10 +53,9 @@ class PPOMemory:
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, n_actions, input_dims, alpha, chkpt_dir='models'):
+    def __init__(self, n_actions, input_dims, alpha):
         super(ActorNetwork, self).__init__()
 
-        self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo_14')
         self.actor = nn.Sequential(
                 nn.Linear(input_dims, 64),
                 nn.ReLU(),
@@ -73,19 +75,18 @@ class ActorNetwork(nn.Module):
         
         return dist
 
-    def save_checkpoint(self):
-        torch.save(self.state_dict(), self.checkpoint_file)
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file)
 
-    def load_checkpoint(self):
-        self.load_state_dict(torch.load(self.checkpoint_file))
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))
 
 
 
 class CriticNetwork(nn.Module):
-    def __init__(self, input_dims, alpha, chkpt_dir='models'):
+    def __init__(self, input_dims, alpha):
         super(CriticNetwork, self).__init__()
 
-        self.checkpoint_file = os.path.join(chkpt_dir, 'critic_torch_ppo_14')
         self.critic = nn.Sequential(
                 nn.Linear(input_dims, 64),
                 nn.ReLU(),
@@ -103,11 +104,11 @@ class CriticNetwork(nn.Module):
 
         return value
 
-    def save_checkpoint(self):
-        torch.save(self.state_dict(), self.checkpoint_file)
+    def save_checkpoint(self, checkpoint):
+        torch.save(self.state_dict(), checkpoint)
 
-    def load_checkpoint(self):
-        self.load_state_dict(torch.load(self.checkpoint_file))
+    def load_checkpoint(self, checkpoint):
+        self.load_state_dict(torch.load(checkpoint))
 
 
 #actor="normal", n_actions=4, input_dims=8, gamma=0.99, alpha=0.0003, gae_lambda=0.95, policy_clip=0.2, batch_size=64, n_epochs=10
@@ -123,20 +124,23 @@ class PPOAgent:
         self.actor = ActorNetwork(config['n_actions'], config['input_dims'], self.alpha)
         
         self.critic = CriticNetwork(config['input_dims'], self.alpha)
+        logger.log("info", f"{self.name} model is initialized")
         self.memory = PPOMemory(config['batch_size'])
        
     def remember(self, state, next_state, action, probs, vals, reward, done):
         self.memory.store_memory(state, next_state, action, probs, vals, reward, done)
 
-    def save_models(self):
+    def save_models(self, checkpoint):
         #print('... saving models ...')
-        self.actor.save_checkpoint()
-        self.critic.save_checkpoint()
+        actor_path = os.path.join(checkpoint, "actor.pth")
+        critic_path = os.path.join(checkpoint, "critic.pth")
+        self.actor.save_checkpoint(actor_path)
+        self.critic.save_checkpoint(critic_path)
 
-    def load_models(self):
+    def load_models(self, checkpoint):
         #print('... loading models ...')
-        self.actor.load_checkpoint()
-        self.critic.load_checkpoint()
+        self.actor.load_checkpoint(checkpoint)
+        self.critic.load_checkpoint(checkpoint)
 
     def choose_action(self, observation):
         state = torch.tensor([observation], dtype=torch.float).to(self.actor.device)
@@ -209,4 +213,181 @@ class PPOAgent:
                 self.critic.optimizer.step()
 
         self.memory.clear_memory()
+
+
+
+
+# ===========================================================================================================================================================================
+# ============================================================================ KAN-PPO  =====================================================================================
+# ===========================================================================================================================================================================
+
+
+class KANActorNetwork(nn.Module):
+    def __init__(self, n_actions, input_dims, alpha):
+        super(KANActorNetwork, self).__init__()
+
+        self.actor = nn.Sequential(
+                KANLayer([input_dims, 128, 256]),  
+                nn.LayerNorm(256),
+                KANLayer([256, 128, n_actions]), 
+                nn.Softmax(dim=-1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        dist = self.actor(state)
+        dist = Categorical(dist)
+        
+        return dist
+
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file)
+
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))
+
+
+
+class KANCriticNetwork(nn.Module):
+    def __init__(self, input_dims, alpha):
+        super(KANCriticNetwork, self).__init__()
+
+        self.critic = nn.Sequential(
+                KANLayer([input_dims, 128, 256]),
+                nn.LayerNorm(256),
+                KANLayer([256, 128, 1]),
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        value = self.critic(state)
+
+        return value
+
+    def save_checkpoint(self, checkpoint):
+        torch.save(self.state_dict(), checkpoint)
+
+    def load_checkpoint(self, checkpoint):
+        self.load_state_dict(torch.load(checkpoint))
+
+
+#actor="normal", n_actions=4, input_dims=8, gamma=0.99, alpha=0.0003, gae_lambda=0.95, policy_clip=0.2, batch_size=64, n_epochs=10
+class KANPPOAgent:
+    def __init__(self, config):
+        self.gamma = config['gamma']
+        self.policy_clip = config['policy_clip']
+        self.n_epochs = config['n_epochs']
+        self.gae_lambda = config['gae_lambda']
+        self.name = "KAN-PPO-Agent"
+        self.alpha = config['alpha']
+
+        self.actor = KANActorNetwork(config['n_actions'], config['input_dims'], self.alpha)
+
+        self.critic = KANCriticNetwork(config['input_dims'], self.alpha)
+        logger.log("info", f"{self.name} model initialized")
+
+        self.memory = PPOMemory(config['batch_size'])
+       
+
+    def remember(self, state, next_state, action, probs, vals, reward, done):
+        self.memory.store_memory(state, next_state, action, probs, vals, reward, done)
+
+
+    def save_models(self, checkpoint):
+        #print('... saving models ...')
+        actor_path = os.path.join(checkpoint, "kan_actor.pth")
+        critic_path = os.path.join(checkpoint, "kan_critic.pth")
+        self.actor.save_checkpoint(actor_path)
+        self.critic.save_checkpoint(critic_path)
+
+
+    def load_models(self, checkpoint):
+        #print('... loading models ...')
+        self.actor.load_checkpoint(checkpoint)
+        self.critic.load_checkpoint(checkpoint)
+
+
+    def choose_action(self, observation):
+        state = torch.tensor([observation], dtype=torch.float).to(self.actor.device)
+
+        dist = self.actor(state)
+        value = self.critic(state)
+        action = dist.sample()
+
+        probs = torch.squeeze(dist.log_prob(action)).item()
+        action = torch.squeeze(action).item()
+        value = torch.squeeze(value).item()
+
+        return action, probs, value
+    
+
+    def learn(self):
+        for _ in range(self.n_epochs):
+            state_arr, next_state_arr, action_arr, old_prob_arr, vals_arr,\
+            reward_arr, dones_arr, batches = \
+                    self.memory.generate_batches()
+
+            values = vals_arr
+            advantage = np.zeros(len(reward_arr), dtype=np.float32)
+
+            for t in range(len(reward_arr)-1):
+                discount = 1
+                a_t = 0
+                for k in range(t, len(reward_arr)-1):
+                    a_t += discount*(reward_arr[k] + self.gamma*values[k+1]*\
+                            (1-int(dones_arr[k])) - values[k])
+                    discount *= self.gamma*self.gae_lambda
+                advantage[t] = a_t
+            
+            #normalize the advantage (improvements)
+            #advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
+            advantage = torch.tensor(advantage).to(self.actor.device)
+
+            values = torch.tensor(values).to(self.actor.device)
+            for batch in batches:
+                states = torch.tensor(state_arr[batch], dtype=torch.float).to(self.actor.device)
+                next_states = torch.tensor(next_state_arr[batch], dtype=torch.float).to(self.actor.device)
+                old_probs = torch.tensor(old_prob_arr[batch]).to(self.actor.device)
+                actions = torch.tensor(action_arr[batch]).to(self.actor.device)
+
+                dist = self.actor(states)
+                critic_value = self.critic(states)
+
+                critic_value = torch.squeeze(critic_value)
+                next_value = self.critic(next_states)
+
+                new_probs = dist.log_prob(actions)
+                prob_ratio = new_probs.exp() / old_probs.exp()
+                #prob_ratio = (new_probs - old_probs).exp()
+                weighted_probs = advantage[batch] * prob_ratio
+                weighted_clipped_probs = torch.clamp(prob_ratio, 1-self.policy_clip,
+                        1+self.policy_clip)*advantage[batch]
+                actor_loss = -torch.min(weighted_probs, weighted_clipped_probs).mean()
+
+                #returns = reward_arr + 0.99 * next_value * (1 - done_arr)
+                returns = advantage[batch] + values[batch]
+                critic_loss = (returns-critic_value)**2
+                critic_loss = critic_loss.mean()
+
+
+
+                #entropy = dist.entropy().mean()
+                #total_loss = actor_loss + 0.5 * critic_loss - 0.01 * entropy
+
+
+                total_loss = actor_loss + 0.5*critic_loss
+                self.actor.optimizer.zero_grad()
+                self.critic.optimizer.zero_grad()
+                total_loss.backward()
+                self.actor.optimizer.step()
+                self.critic.optimizer.step()
+
+        self.memory.clear_memory()
+
 
