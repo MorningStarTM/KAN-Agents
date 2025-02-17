74712bc0ce3f935301dc331bd6e0f77dfaeb455b
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-02-17 21:52:50 +0530

    utils for DT

---
+++ offline_agent/utils.py
@@ -0,0 +1,30 @@
+import random
+import time
+import pickle
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from offline_agent.d4rl_info import REF_MAX_SCORE, REF_MIN_SCORE, D4RL_DATASET_STATS
+
+
+
+def discount_cumsum(x, gamma):
+    disc_cumsum = np.zeros_like(x)
+    disc_cumsum[-1] = x[-1]
+    for t in reversed(range(x.shape[0]-1)):
+        disc_cumsum[t] = x[t] + gamma * disc_cumsum[t+1]
+    return disc_cumsum
+
+
+
+def get_d4rl_normalized_score(score, env_name):
+    env_key = env_name.split('-')[0].lower()
+    assert env_key in REF_MAX_SCORE, f'no reference score for {env_key} env to calculate d4rl score'
+    return (score - REF_MIN_SCORE[env_key]) / (REF_MAX_SCORE[env_key] - REF_MIN_SCORE[env_key])
+
+
+def get_d4rl_dataset_stats(env_d4rl_name):
+    return D4RL_DATASET_STATS[env_d4rl_name]
+
+
+
